3579d4b844294d23fb4e2d9bf85355be35a99fda
colo124 <mauro.scarp@gmail.com>
2016-12-18 15:32:14 -0300

    as

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -7,14 +7,7 @@ return [
             'username' => 'zbsezklgjldxgj',
             'password' => 'e6d21956806b6f2e9ba00f6bf71885d64385b56fb2027ee8e0d37c682fe317cc',
             'charset' => 'utf8',
-        ],/*
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=scarpadb',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],*/
+        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
